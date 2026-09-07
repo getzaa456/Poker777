2b0c08d846a0462092c496cc64b4c2cab467458e
JapanSG <thanakrit.sri45326@gmail.com>
2026-09-07 19:12:23 +0700

    Add frontend server - Add CORS to allow frontend local dev server - Move all static file to the public folder - Install express

---
+++ frontend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = Number(process.env.PORT) || 3000;
+const publicDirectory = path.join(__dirname, 'public');
+
+app.use(express.static(publicDirectory));
+
+app.get('/', (_req, res) => {
+	res.sendFile(path.join(publicDirectory, 'index.html'));
+});
+
+app.get('/lobby', (_req, res) => {
+	res.sendFile(path.join(publicDirectory, 'lobby.html'));
+});
+
+app.get('/poker-table', (_req, res) => {
+	res.sendFile(path.join(publicDirectory, 'poker-table.html'));
+});
+
+app.listen(port, () => {
+	console.log(`Poker777 frontend listening on http://localhost:${port}`);
+});
+
+module.exports = app;
